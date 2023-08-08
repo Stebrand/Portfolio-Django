37c1b5d72a67b166da5990b831b68a370eed42e4
Stebrand <sbr16@hotmail.es>
2023-08-07 19:45:08 -0500

    Incial app blog back and front

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.db.models.fields import CharField, TextField, DateField
+from django.db.models.fields.files import ImageField
+import datetime
 # Create your models here.
+
+class Post(models.Model):
+    title = CharField(max_length=100)
+    description = TextField()
+    image = ImageField(upload_to='blog/images')
+    date = DateField(datetime.date.today)
